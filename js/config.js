2aaa2007293346e2c68374cb07289b24db1603e3
Ido Green <greenido@gmail.com>
2012-09-09 11:56:00 +0300

    initial version

---
+++ js/config.js
@@ -0,0 +1,23 @@
+window.slidesConfig = window.slidesConfig || {
+  // Slide settings
+  settings : {
+    useBuilds: true,
+    useGDDBranding: false,
+    includeWebRTC: true,
+    hashtag: '#HTML5'
+  },
+  info: {
+    // Personal info
+    name: 'Ido Green',
+    pic: 'https://lh4.googleusercontent.com/-YpY1A_LkwH8/AAAAAAAAAAI/AAAAAAAAGPY/hpy-JaS2PtQ/photo.jpg?sz=200',
+    gplus: 'https://plus.google.com/101709676645745930035/',
+    twitter: 'greenido',
+    blog: 'http://greenido.wordpress.com/',
+
+    // GDD info
+    location: 'London, UK',
+    date: new Date('Sep 14 2012'),
+    logo: 'images/chrome-israel-logo-tiny.png'
+    
+  }
+};
